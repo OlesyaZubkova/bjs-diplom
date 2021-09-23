cbe7bb119a82f74ad785d4e26416bb295371f9d5
Olesya Zubkova <olesya_2802@mail.ru>
2021-09-23 19:49:57 +0300

    loginPage added

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,13 +2,13 @@
 
 const userForm = new UserForm();
 
-userForm.loginFormCallback = (data) => {
+userForm.loginFormCallback = data => {
     ApiConnector.login(data, response => {
         response.success === true ? location.reload() : userForm.setLoginErrorMessage(response.error);
     });
 };
 
-userForm.registerFormCallback = (data) => {
+userForm.registerFormCallback = data => {
     ApiConnector.register(data, response => {
         response.success === true ? location.reload() : userForm.setRegisterErrorMessage(response.error);
     });
