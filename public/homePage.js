37edd6199201fde5a9ac9d15923c21735fbaf29e
Olesya Zubkova <olesya_2802@mail.ru>
2021-10-04 20:36:41 +0300

    homePage changed

--- public/homePage.js
+++ public/homePage.js
@@ -19,7 +19,7 @@ const ratesBoard = new RatesBoard();
 
 function getRatesBoard () {
     ApiConnector.getStocks(response => {
-        if (response === true) {
+        if (response.success === true) {
             ratesBoard.clearTable();
             ratesBoard.fillTable(response.data);
         }
@@ -34,34 +34,63 @@ const moneyManager = new MoneyManager();
 
 moneyManager.addMoneyCallback = (data) => {
     ApiConnector.addMoney(data, response => {
-        if (response === true) {
+        if (response.success === true) {
             ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(response.success, 'Счет пополнен');
         } else {
-            moneyManager.setMessage(response.error, 'Не удалось пополнить счет');
+            moneyManager.setMessage(response.success, response.error);
         }
     });
 };
 
 moneyManager.conversionMoneyCallback = (data) => {
     ApiConnector.convertMoney(data, response => {
-        if (response === true) {
+        if (response.success === true) {
             ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(response.success, 'Конвертирование валюты прошло успешно');
         } else {
-            moneyManager.setMessage(response.error, 'Не удалось произвести конвертацию');
+            moneyManager.setMessage(response.success, response.error);
         }
     });
 };
 
 moneyManager.sendMoneyCallback = (data) => {
     ApiConnector.transferMoney(data, response => {
-        if (response === true) {
+        if (response.success === true) {
             ProfileWidget.showProfile(response.data);
             moneyManager.setMessage(response.success, 'Перевод выполнен успешно');
         } else {
-            moneyManager.setMessage(response.error, 'Не удалось выполнить перевод');
+            moneyManager.setMessage(response.success, response.error);
         }            
     });
 };
 
+const favoriteWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response => {
+    if (response.success === true) {
+        favoriteWidget.clearTable();
+        favoriteWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+});
+
+favoriteWidget.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success === true) {
+            favoriteWidget.setMessage(response.success, 'Пользователь добавлен в избранное');
+        } else {
+            favoriteWidget.setMessage(response.success, response.error);
+        }     
+    });
+};
+
+favoriteWidget.removeUserCallback  = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+            if (response.success === true) {
+                favoriteWidget.setMessage(response.success, 'Пользователь удален из избранного');
+            } else {
+                favoriteWidget.setMessage(response.success, response.error);           
+        }
+    });
+};
